23647c922d579942396a43f2b47bcfd25bc32831
bahadirsina <bahadirsinaterzioglu01@gmail.com>
2026-06-01 17:58:18 +0300

    Update

--- microservices/user-service/src/main/java/com/turkcell/user_service/controller/UserController.java
+++ microservices/user-service/src/main/java/com/turkcell/user_service/controller/UserController.java
@@ -2,12 +2,15 @@ package com.turkcell.user_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/api/users")
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello customer-service";
+    @GetMapping
+    public String get() {
+        System.out.println("UsersController çalıştı");
+        return "UsersController";
     }
 }
